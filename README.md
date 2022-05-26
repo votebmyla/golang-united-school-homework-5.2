2d5d2f7b01dd987dd08580554ad607f7f082fdf0
votebmyla <votebmyla@gmail.com>
2022-05-27 03:02:49 +0500

    add solution

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Cache
 
-Implement a simple cache, that holds values with type `string` and allows to retrieve them using keys of time `stings`. Key/value pairs can expire if given a deadline using an appropriate method. 
+Implement a simple cache, that holds values with type `string` and allows to retrieve them using keys of type `string`. Key/value pairs can expire if given a deadline using an appropriate method.
 
-The initial structure is given. You should add appropriate fields, implement the constructor function and methods with given signatures. 
+The initial structure is given. You should add appropriate fields, implement the constructor function and methods with given signatures.
 
 `k, ok := Get(key string)` - returns the value associated with the key and the boolean `ok` (true if exists, false if not), if the deadline of the key/value pair has not been exceeded yet
-
 
 `Put(key string, value string)` places a value with an associated key into cache. Value put with this method never expired(have infinite deadline). Putting into the existing key should overwrite the value
 
@@ -17,5 +16,3 @@ Should do the same as `Put`, but the key/value pair should expire by given deadl
 The expiration should, and probably cannot in our case, be implemented asynchronously, so think of a way to clean up the records when needed
 
 NOTE: receiver is a placeholder, replace appropriately
-
-
--- cache.go
+++ cache.go
@@ -3,21 +3,73 @@ package cache
 import "time"
 
 type Cache struct {
+	KeyVal []KeyValue
+}
+
+type KeyValue struct {
+	Key      string
+	Value    string
+	Deadline time.Time
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	now := time.Now()
 
+	for i, v := range c.KeyVal {
+		if key == c.KeyVal[i].Key && now.Before(v.Deadline) {
+			return v.Value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	deadline := time.Time{}
+
+	for i, v := range c.KeyVal {
+		if key == v.Key {
+			c.KeyVal[i].Value = value
+			c.KeyVal[i].Deadline = deadline
+			return
+		}
+	}
+
+	data := KeyValue{
+		Key:      key,
+		Value:    value,
+		Deadline: deadline,
+	}
+	c.KeyVal = append(c.KeyVal, data)
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := []string{}
+	now := time.Now()
+	for _, v := range c.KeyVal {
+		if now.Before(v.Deadline) || v.Deadline.Equal(time.Time{}) {
+			keys = append(keys, v.Key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	for _, v := range c.KeyVal {
+		if v.Key == key {
+			v.Value = value
+			v.Deadline = deadline
+			return
+		}
+	}
+
+	data := KeyValue{
+		Key:      key,
+		Value:    value,
+		Deadline: deadline,
+	}
+	c.KeyVal = append(c.KeyVal, data)
 }
