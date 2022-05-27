548929469864d358508af92386d756172214b05b
votebmyla <votebmyla@gmail.com>
2022-05-27 23:25:05 +0500

    fix

--- cache.go
+++ cache.go
@@ -12,11 +12,10 @@ type KeyValue struct {
 }
 
 func NewCache() Cache {
-	cache := Cache{KeyVal: make(map[string]KeyValue)}
-	return cache
+	return Cache{KeyVal: map[string]KeyValue{}}
 }
 
-func (c *Cache) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
 	for k, v := range c.KeyVal {
 		if k == key && v.Deadline.After(time.Now()) {
 			return v.Value, true
@@ -25,7 +24,7 @@ func (c *Cache) Get(key string) (string, bool) {
 	return "", false
 }
 
-func (c *Cache) Put(key, value string) {
+func (c Cache) Put(key, value string) {
 	deadline := time.Time{}
 	for k := range c.KeyVal {
 		if k == key {
@@ -36,7 +35,7 @@ func (c *Cache) Put(key, value string) {
 	c.KeyVal[key] = KeyValue{value, deadline}
 }
 
-func (c *Cache) Keys() []string {
+func (c Cache) Keys() []string {
 	keys := []string{}
 	for k, v := range c.KeyVal {
 		if v.Deadline.After(time.Now()) || v.Deadline.Equal(time.Time{}) {
@@ -46,6 +45,6 @@ func (c *Cache) Keys() []string {
 	return keys
 }
 
-func (c *Cache) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 	c.KeyVal[key] = KeyValue{value, deadline}
 }
