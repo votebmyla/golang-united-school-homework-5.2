2787f9da8a8eed7094e4afc8fbf46e3386047537
votebmyla <votebmyla@gmail.com>
2022-05-27 23:56:58 +0500

    Revert "z"
    This reverts commit d025f89fa0ede82cb92a1ef55faed50246c581d9.

--- cache.go
+++ cache.go
@@ -3,7 +3,7 @@ package cache
 import "time"
 
 type Cache struct {
-	KeyVal map[string]*KeyValue
+	KeyVal map[string]KeyValue
 }
 
 type KeyValue struct {
@@ -12,8 +12,7 @@ type KeyValue struct {
 }
 
 func NewCache() Cache {
-	c := Cache{KeyVal: make(map[string]*KeyValue)}
-	return c
+	return Cache{KeyVal: map[string]KeyValue{}}
 }
 
 func (c Cache) Get(key string) (string, bool) {
@@ -26,7 +25,7 @@ func (c Cache) Get(key string) (string, bool) {
 }
 
 func (c Cache) Put(key, value string) {
-	c.KeyVal[key] = &KeyValue{value, time.Time{}}
+	c.KeyVal[key] = KeyValue{value, time.Time{}}
 }
 
 func (c Cache) Keys() []string {
@@ -40,5 +39,5 @@ func (c Cache) Keys() []string {
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
-	c.KeyVal[key] = &KeyValue{value, deadline}
+	c.KeyVal[key] = KeyValue{value, deadline}
 }
